62e3c1116abe25a224a63360a8f8fc6ff72ee464
Yegor_Karaev <154316908+YgorKa97@users.noreply.github.com>
2023-12-27 15:49:27 +0200

    Update Divisors.java

--- Divisors.java
+++ Divisors.java
@@ -5,9 +5,10 @@ public class Divisors {
 	public static void main (String[] args) {
 		//// Put your code here
 		int x = Integer.parseInt(args[0]);
-        for(int i = 1; i <= x; i++){
-            if(x % i ==0) {
-                System.out.println(i + " ");
-            }
+        	for(int i = 1; i <= x; i++){
+           		if(x % i ==0) {
+               		 System.out.println(i + " ");
+	    		}
+        	}
 	}
 }

7b9fa9f811593a212c91a517dba4b0cba2a5c028
Yegor_Karaev <154316908+YgorKa97@users.noreply.github.com>
2023-12-27 18:50:59 +0200

    Update Perfect.java

--- Perfect.java
+++ Perfect.java
@@ -3,6 +3,27 @@
  */
 public class Perfect {
 	public static void main (String[] args) {
-		//// Put your code here
+		 // user input
+        int num = Integer.parseInt(args[0]);
+        // sum = 1 because String starts from 1
+        int sum = 1;
+        // Start string from 1 because we need to make it look like this:
+        // 6 is a perfect number since 6 = 1 + 2 + 3
+        String divisors = "1";
+        //start for loop to find divisors i = 2 because sum starts from 1
+        for(int i = 2; i < num; i++){
+            //check for divisors
+            if(num % i == 0){
+                sum += i;
+                // String starts from 1 and for loop ads divisors.
+                divisors += " + " + i;
+            }
+        }
+        // check for a perfect number or not
+        if (sum == num){
+            System.out.println(num + " is a perfect number since " + num + " = " + divisors );
+        }else{
+            System.out.println(num + "  is not a perfect number");
+        }
 	}
 }

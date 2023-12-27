0288325580a4c6579220f03f45e5a5528db52246
Yegor_Karaev <154316908+YgorKa97@users.noreply.github.com>
2023-12-27 15:37:22 +0200

    Update Reverse.java

--- Reverse.java
+++ Reverse.java
@@ -5,12 +5,14 @@
 public class Reverse {
 	public static void main (String[] args){
 		//// Put your code here
-		String word = args[0];
-			for(int i = word.length() - 1; i >= 0; i--){
-				System.out.print(word.charAt(i));
-			}
-		System.out.println();
-		int middleIndex = word.length() / 2;
-		System.out.println("The middle character is: " + word.charAt(middleIndex));	
+		   String word = args[0];
+        for( int i = word.length() - 1; i >= 0; i--){
+            System.out.print(word.charAt(i));
+        }
+        System.out.println();
+        int middleIndex = word.length() / 2;
+            System.out.println( "The middle character is: " + word.charAt(middleIndex));
+    }
+}
 	}
 }

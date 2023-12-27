cf7d452aaf8a6983a262cde07406961599ef6783
Yegor_Karaev <154316908+YgorKa97@users.noreply.github.com>
2023-12-27 16:51:33 +0200

    Update InOrder.java

--- InOrder.java
+++ InOrder.java
@@ -5,5 +5,21 @@
 public class InOrder {
 	public static void main (String[] args) {
 		//// Write your code here
-	}
+		// make a inr previous = 0
+		int p = 0;
+	//start while loop until random >= p, if random < p, loop breaks	
+        while(true){
+	    // generate random number in range [0 -> 10)
+            int random = (int) (Math.random() * 10);
+            if(random >= p){
+                System.out.print(random + " ");
+		// previous number gets random value fron last loop
+                p = random;
+            }else{
+		// breaking loop if random <= p
+                break;
+            }
+        }
+    }
 }
+

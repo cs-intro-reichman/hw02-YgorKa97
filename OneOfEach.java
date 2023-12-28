4ec1a2b776c90bad4687b27573b9cdd2f0fa985c
Yegor_Karaev <154316908+YgorKa97@users.noreply.github.com>
2023-12-28 15:45:17 +0200

    Update OneOfEach.java

--- OneOfEach.java
+++ OneOfEach.java
@@ -5,6 +5,26 @@
  */
 public class OneOfEach {
 	public static void main (String[] args) {
-		//// Put your code here
+		// add 2 boolean variables, to keep track of whether a boy or girl has been born
+        boolean isBoy = false;
+        boolean isGirl = false;
+        int childrenCount = 0;
+        // while(true || true) Count until there is at least one boy and one girl
+        while(!isGirl || !isBoy){
+            //50% chance for boy and girl
+            boolean hasBoy = Math.random() > 0.5;
+            // if hasBoy = true, loop going on
+            if(hasBoy){
+                isBoy = true;
+                System.out.print("b ");
+            // isGirl = true, loop is on
+            }else{
+                isGirl = true;
+                System.out.print("g ");
+            }
+            childrenCount++;
+        }
+        System.out.println("\nYou made it... and you now have " + childrenCount + " children.");
+
 	}
 }

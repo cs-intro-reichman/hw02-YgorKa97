0a26b3702581ef06522bfb601b1e89e4d86d5c31
Yegor_Karaev <154316908+YgorKa97@users.noreply.github.com>
2023-12-28 10:54:16 +0200

    Update DamkaBoard.java

--- DamkaBoard.java
+++ DamkaBoard.java
@@ -3,10 +3,10 @@
  */
 public class DamkaBoard {
 	public static void main(String[] args) {
-		int board = Integer.parseInt(args[0]);
+		 int board = Integer.parseInt(args[0]);
         for( int i = 0; i < board; i++){
-            for (int j = -3; j <= board; j++) {
-                if( (i+j) % 2 != 0){
+            for (int j = (1-board); j <= board; j++) {
+                if( (i+j) % 2 == 0){
                     System.out.print("*");
                 }else{
                     System.out.print(" ");

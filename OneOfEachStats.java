c9d93714a94fb1e554903a6c0928189337c16b9a
Yegor_Karaev <154316908+YgorKa97@users.noreply.github.com>
2023-12-30 14:04:51 +0200

    Update OneOfEachStats.java

--- OneOfEachStats.java
+++ OneOfEachStats.java
@@ -9,11 +9,47 @@ import java.util.Random;
  */
 public class OneOfEachStats {
 	public static void main (String[] args) {
-		// Gets the two command-line arguments
 		int T = Integer.parseInt(args[0]);
-		int seed = Integer.parseInt(args[1]);
+        int seeds = Integer.parseInt(args[1]);
+        int totalChildren = 0;
+        int twoChildren = 0;
+        int threeChildren = 0;
+        int fourOrMore = 0;
+        Random generator = new Random(seeds);
+        for(int i = 0; i<=T; i++){
+            boolean isBoy = false;
+            boolean isGirl = false;
+            int childCount = 0;
+            while(!isBoy || !isGirl){
+                double rnd = generator.nextDouble();
+                boolean hasBoy = rnd < 0.5;
+                if(hasBoy){
+                    isBoy = true;
+                }else{
+                    isGirl = true;
+                }
+                childCount++;
+            }
+            totalChildren += childCount;
+
+            if(childCount == 2){
+                twoChildren++;
+            } else if (childCount == 3){
+                threeChildren++;
+            }else if (childCount >= 4){
+                fourOrMore++;
+            }
+        }
+        double avg = (double) totalChildren / T;
+        System.out.println("Average: " + avg + " children to get at least one of each gender.");
+        System.out.println("Number of families with 2 children: " + twoChildren);
+        System.out.println("Number of families with 3 children: " + threeChildren);
+        System.out.println("Number of families with 4 children or more: " + fourOrMore);
+		// Gets the two command-line arguments
+		//int T = Integer.parseInt(args[0]);
+		//int seed = Integer.parseInt(args[1]);
 		// Initailizes a random numbers generator with the given seed value
-        Random generator = new Random(seed);  
+        //Random generator = new Random(seed);  
 		
 		//// In the previous version of this program, you used a statement like:
 		//// double rnd = Math.random();
